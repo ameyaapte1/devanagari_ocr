359e1d17940ae042d760d9bbeb665312825939ef
Ameya Apte <ameyaapte1@gmail.com>
2017-03-24 17:22:21 +0530

    removed unwanted files

--- erosion.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-img = cv2.imread('binary.jpg',cv2.IMREAD_GRAYSCALE)
-#img=255-img
-kernel = np.ones((3,3),np.uint8)
-#gradient = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel)
-dilation = cv2.erode(img,kernel,iterations = 1)
-cv2.imwrite("erosion.jpg",dilation)
